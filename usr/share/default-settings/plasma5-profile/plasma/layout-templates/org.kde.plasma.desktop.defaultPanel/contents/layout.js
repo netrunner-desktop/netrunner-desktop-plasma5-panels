6c17ea0ae64f34269fe540a92b0a0be94e370949
Rohan Garg <rohan@garg.io>
2016-07-28 14:42:30 +0200

    Fix systray configuration

--- usr/share/default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
+++ usr/share/default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -50,9 +50,11 @@ yakuakeIcon.writeConfig("iconName", "yakuake")
 yakuakeIcon.writeConfig("url", "/usr/share/applications/org.kde.yakuake.desktop")
 
 var systray = panel.addWidget("org.kde.plasma.systemtray")
-systray.currentConfigGroup = ["General"]
-systray.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.networkmanagement,org.kde.plasma.clipboard,org.kde.plasma.volume,org.kde.discovernotifier,org.kde.plasma.diskquota")
-systray.writeConfig("knownItems", "org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.discovernotifier,org.kde.plasma.clipboard,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.printmanager,org.kde.plasma.notifications")
+var systrayContainmentId = systray.readConfig("SystrayContainmentId")
+var systrayContainment = desktopById(systrayContainmentId)
+systrayContainment.currentConfigGroup = ["General"]
+systrayContainment.writeConfig("extraItems","org.kde.plasma.devicenotifier,org.kde.plasma.networkmanagement,org.kde.plasma.clipboard,org.kde.plasma.volume,org.kde.discovernotifier,org.kde.plasma.diskquota")
+systrayContainment.writeConfig("knownItems", "org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.discovernotifier,org.kde.plasma.clipboard,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.printmanager,org.kde.plasma.notifications")
 
 
 panel.addWidget("org.kde.plasma.digitalclock")
