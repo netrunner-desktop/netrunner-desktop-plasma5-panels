566073f04cbfd9c9d4721dc890b7b047ea064d48
Rohan Garg <rohan@garg.io>
2016-07-29 16:00:15 +0200

    Switch to kickerdash
    Ref https://github.com/maui-linux/issues/issues/27

--- usr/share/default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
+++ usr/share/default-settings/plasma5-profile/plasma/layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -27,7 +27,7 @@ if (freeEdges["bottom"] == true) {
 
 panel.height = screenGeometry(panel.screen).height > 1024 ? 35 : 27
 
-var simplemenu = panel.addWidget("org.kde.plasma.simplemenu")
+var simplemenu = panel.addWidget("org.kde.plasma.kickerdash")
 simplemenu.currentConfigGroup = ["General"]
 simplemenu.writeConfig("favoriteApps", "org.kde.ksysguard.desktop,systemsettings.desktop,org.kde.konsole.desktop,synaptic.desktop")
 simplemenu.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop")
